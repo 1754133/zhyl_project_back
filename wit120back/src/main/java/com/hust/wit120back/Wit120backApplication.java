61814b66148f040002564e6dc0690891b69f8b96
Huangjinx <1073130610@qq.com>
2022-03-09 11:34:07 +0800

    智慧医疗信息系统后端第一次测试提交

---
+++ wit120back/src/main/java/com/hust/wit120back/Wit120backApplication.java
@@ -0,0 +1,13 @@
+package com.hust.wit120back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wit120backApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wit120backApplication.class, args);
+    }
+
+}
---
+++ wit120back/src/test/java/com/hust/wit120back/Wit120backApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.wit120back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wit120backApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
