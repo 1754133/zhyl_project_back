025c4c22cf27ed1589b5ba83e873fc0bc3fa56d6
Iecy_yl <abcwyy123456@163.com>
2022-03-09 16:35:30 +0800

    This is a git test.

--- wit120back/src/main/java/com/hust/wit120back/Wit120backApplication.java
+++ wit120back/src/main/java/com/hust/wit120back/Wit120backApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Wit120backApplication {
-
+    //git test
     public static void main(String[] args) {
         SpringApplication.run(Wit120backApplication.class, args);
     }
