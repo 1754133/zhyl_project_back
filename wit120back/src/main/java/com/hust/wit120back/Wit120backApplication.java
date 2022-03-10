50a108e5f854228fd6a8867332825de11f848eb9
Iecy_yl <abcwyy123456@163.com>
2022-03-10 15:39:12 +0800

    实现短信发送接口

--- wit120back/src/main/java/com/hust/wit120back/Wit120backApplication.java
+++ wit120back/src/main/java/com/hust/wit120back/Wit120backApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Wit120backApplication {
-    //git test
+
     public static void main(String[] args) {
+
         SpringApplication.run(Wit120backApplication.class, args);
+
     }
+
+
 
 }
