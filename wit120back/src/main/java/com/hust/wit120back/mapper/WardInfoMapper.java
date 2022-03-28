72d07fd04121a04785095ec2864f35058185c22f
Iecy_yl <abcwyy123456@163.com>
2022-03-28 11:30:49 +0800

    根据doctorId查询病人住院信息；删除病人住院信息(v2.0)

--- wit120back/src/main/java/com/hust/wit120back/mapper/WardInfoMapper.java
+++ wit120back/src/main/java/com/hust/wit120back/mapper/WardInfoMapper.java
@@ -3,6 +3,8 @@ package com.hust.wit120back.mapper;
 import com.hust.wit120back.dto.WardInfoDTO;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface WardInfoMapper {
     @Select("select count(*) from ward_info where ward_id = #{wardId}")
@@ -23,4 +25,16 @@ public interface WardInfoMapper {
             @Result(column = "principal_id", property = "principalId")
     })
     WardInfoDTO selectWardInfoByPatientId(Integer patientId);
+
+    @Select("select ward_id, bed_id, patient_id, create_time from ward_info where principal_id = #{doctorId}")
+    @Results({
+            @Result(column = "ward_id", property = "wardId"),
+            @Result(column = "bed_id", property = "bedId"),
+            @Result(column = "patient_id", property = "patientId"),
+            @Result(column = "create_time", property = "createTime")
+    })
+    List<WardInfoDTO> selectPatientWardInfoByDoctorId(Integer doctorId);
+
+    @Delete("delete from ward_info where patient_id = #{patientId}")
+    boolean deleteWardInfoByPatientId(Integer patientId);
 }
